fb0471cd63398e390451b26ba082429f89d64019
Daniel Desira <desiradaniel2007@gmail.com>
2026-02-13 10:26:58 +0100

    refactor check method into 2 simpler private methods

--- src/score.py
+++ src/score.py
@@ -34,19 +34,25 @@ class Score:
     def player_id(self) -> int:
         return self.__player_id
 
-    def check(self, rule_sheet):
+    def __get_levels_possible_total(self, rule_sheet) -> int:
         max_points = 0
-
         for i in range(1, self.__level + 1):
             level_data = rule_sheet.get(f"level{i}")
             if level_data is not None:
                 max_points += level_data.get('characterMax')
                 if i < self.__level or self.__outcome_id == Outcomes.Win.value:
                     max_points += level_data.get('pass')
+        return max_points
 
+    def __get_duration_reward_if_applicable(self, rule_sheet) -> int:
         duration_reward = rule_sheet.get('durationReward')
-        if duration_reward is not None:
-            if self.__duration <= duration_reward.get('durationLimit') and self.__outcome_id == Outcomes.Win.value:
-                max_points += duration_reward.get('reward')
+        if (duration_reward is not None
+                and self.__duration <= duration_reward.get('durationLimit')
+                and self.__outcome_id == Outcomes.Win.value):
+            return duration_reward.get('reward')
+        else:
+            return 0
 
+    def check(self, rule_sheet):
+        max_points = self.__get_levels_possible_total(rule_sheet) + self.__get_duration_reward_if_applicable(rule_sheet)
         return max_points >= self.__points
