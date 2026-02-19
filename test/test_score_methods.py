12ce60fc7428ead2b4d90034a0fb861f2bf1921a
Daniel Desira <desiradaniel2007@gmail.com>
2026-02-19 19:22:36 +0100

    update and prepare new rule sheet structure

--- test/test_score_methods.py
+++ test/test_score_methods.py
@@ -7,17 +7,33 @@ from src.score import Score
 class TestScoreMethods(unittest.TestCase):
     def setUp(self):
         self.__rulesheet = {
-            'level1': {
-                'characterMax': 2000,
-                'pass': 100,
+            'levelRewards': {
+                1: 100,
+                2: 150,
+                3: 150,
             },
-            'level2': {
-                'characterMax': 2500,
-                'pass': 150,
+            'interactionRewards': {
+                'testPositive1': 5,
+                'testNegative1': -5,
+                'testPositive2': 10,
+                'testNegative2': -10,
+                'testPositive3': 15,
+                'testNegative3': -15,
             },
-            'level3': {
-                'characterMax': 2500,
-                'pass': 150,
+            'levelMaxInteractions': {
+                1: {
+                    'testPositive1': 10,
+                    'testPositive2': 10,
+                },
+                2: {
+                    'testPositive1': 20,
+                    'testPositive2': 10,
+                },
+                3: {
+                    'testPositive1': 20,
+                    'testPositive2': 20,
+                    'testPositive3': 5,
+                },
             },
             'durationReward': {
                 'durationLimit': 200,
